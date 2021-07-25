6b0eb55ccafb2a0f6317d904e832cf79713f03d3
Praveen <praveen.kumar@crownit.in>
2021-07-25 20:48:08 +0530

    segration of emitter service as independent microservice

---
+++ EmitterService/config/config.js
@@ -0,0 +1,13 @@
+const config={
+    ENV:process.env.NODE_ENV,
+    app:{
+        EMITTER_SERVICE_PORT:process.env.EMITTER_SERVICE_PORT,
+        MESSAGE_CHECKSUM_KEY:process.env.MESSAGE_CHECKSUM_KEY,
+        SECRET_KEY:process.env.SECRET_KEY,
+        IV:process.env.IV,
+        MESSAGE_MIN_COUNT:process.env.MESSAGE_MIN_COUNT,
+        MESSAGE_MAX_COUNT:process.env.MESSAGE_MAX_COUNT,
+        EMITTER_SERVICE_INTERVAL:process.env.EMITTER_SERVICE_INTERVAL
+    }
+}
+module.exports = config;
